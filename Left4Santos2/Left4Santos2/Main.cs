31993fa38dc217fc2e2a7a63c36ae3517f587217
akshat2602 <35724794+akshat2602@users.noreply.github.com>
2020-01-12 17:27:05 +0530

    Added basic menu ui

--- Left4Santos2/Left4Santos2/Main.cs
+++ Left4Santos2/Left4Santos2/Main.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GTA;
+﻿using GTA;
 namespace Left4Santos2
 {
     public class Main
     {
-        //lauda
+        MenuPool zombiemod_menupool;
+
     }
 }
--- Left4Santos2/Left4Santos2/Menu.cs
+++ Left4Santos2/Left4Santos2/Menu.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+using NativeUI;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Left4Santos2
 {
-    class Menu
+    public class Menu : Script
     {
+        MenuPool zombiemod_menupool;
+        UIMenu mainMenu;
+
+        public Menu()
+        {
+            zombiemod_menupool = new MenuPool();
+            mainMenu = new UIMenu("Zombie Mod", "Hello there!");
+            zombiemod_menupool.Add(mainMenu);
+        }
+
+        void onTick(object sender, EventArgs e)
+        {
+            if (zombiemod_menupool != null)
+                zombiemod_menupool.ProcessMenus();
+        }
+
+        void onKeyDown(object sender, KeyEventArgs e)
+        {
+            mainMenu.Visible = !mainMenu.Visible;
+        }
     }
 }
