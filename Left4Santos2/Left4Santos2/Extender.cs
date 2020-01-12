14b9e4a4256543d1a0770b3efac74656629e8e67
Rugz007 <dsweb007@gmail.com>
2020-01-12 18:14:12 +0530

    Made Extender Script and worked on Main script

---
+++ Left4Santos2/Left4Santos2/Extender.cs
@@ -0,0 +1,43 @@
+﻿using GTA;
+using GTA.Native;
+namespace Left4Santos2
+{
+    class Extender
+    {
+        public bool CanHearPlayer(Ped ped)
+        {
+            return Function.Call<bool>(Hash.CAN_PED_HEAR_PLAYER,Game.Player.Character,ped);
+        }
+        public bool CanSeePlayer(Ped ped)
+        {
+            return Function.Call<bool>(Hash.HAS_ENTITY_CLEAR_LOS_TO_ENTITY_IN_FRONT, ped, Game.Player.Character);
+        }
+        public void GiveZombieLook(Ped ped)
+        {
+            if (ped != Game.Player.Character)
+            {
+                Function.Call(Hash.STOP_PED_SPEAKING, new InputArgument[]
+                {
+                    ped.Handle,true
+                });
+                Function.Call(Hash.DISABLE_PED_PAIN_AUDIO, new InputArgument[]
+                {
+                    ped.Handle,true
+                });
+                ped.AlwaysKeepTask = true;
+                ped.IsEnemy = true;
+                ped.Health = 3000;
+                Function.Call(Hash.SET_BLOCKING_OF_NON_TEMPORARY_EVENTS, ped, 1);
+                Function.Call(Hash.SET_PED_FLEE_ATTRIBUTES, ped, 0, 0);
+                Function.Call(Hash.SET_PED_COMBAT_ATTRIBUTES, ped, 5, 1);
+                Function.Call(Hash.APPLY_PED_DAMAGE_PACK, ped, "BigHitByVehicle", 0.0, 9.0);
+                Function.Call(Hash.APPLY_PED_DAMAGE_PACK, ped, "SCR_Dumpster", 0.0, 9.0);
+                Function.Call(Hash.APPLY_PED_DAMAGE_PACK, ped, "SCR_Torture", 0.0, 9.0);
+            }
+        }
+        public void SetPopulation(float multiplier)
+        {
+            Function.Call(Hash.SET_PED_DENSITY_MULTIPLIER_THIS_FRAME, multiplier);
+        }
+    }
+}
--- Left4Santos2/Left4Santos2/Main.cs
+++ Left4Santos2/Left4Santos2/Main.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GTA;
+using System.Windows.Forms;
 namespace Left4Santos2 
 {
     public class Main : Script
     {
+        Extender extender;
+        Ped p;
         public Main()
         {
-
+            this.KeyUp += OnKeyUp;
+        }
+        void OnKeyUp(object sender,KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.I)
+            {
+                extender.SetPopulation(0);
+            }
+            if(e.KeyCode == Keys.K)
+            {
+                extender.SetPopulation(5);
+            }
         }
         public void CreateZombie()
         {
@@ -18,7 +32,7 @@ namespace Left4Santos2
         }
         public void MakeEveryoneZombie()
         {
-
+            bool lol = extender.CanHearPlayer(p);
         }
     }
 }
